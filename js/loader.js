cbfe0c47a54ea592b63de8e98b32abb991accf6b
Vladimir Kuznetsov <vladimir.kuznetsov@engine6.com>
2014-02-12 15:57:13 +0600

    demo code

---
+++ js/loader.js
@@ -0,0 +1,45 @@
+(function () {
+
+	/**
+	 * Conditional loader
+	 * @param {String} name of the assets package
+	 * @param {String} mq (e.g. "all and (min-width: 720px)")
+	 * @param {String|Array} files to load
+	 */
+	App.loader = function (name, mq, files) {
+		var mql = window.matchMedia(mq);
+		if (mql.matches) {
+			loader();
+		} else {
+			mql.addListener(loader);
+		}
+		function loader() {
+			if (App.loader[name]) return;
+
+			var i, result = true;
+			for (i = 0; i < files.length; i++) {
+				result = result && /\.css(\?.*)?$/.test(files[i]);
+			}
+			if (result) {
+				for (i = 0; i < files.length; i++) {
+					yepnope.injectCss(files[i]);
+				}
+				LL.loader[name] = 2;
+				return;
+			}
+
+			App.loader[name] = 1;
+			yepnope({
+				load: files,
+				complete: function () {
+					App.loader[name] = 2;
+					mql.removeListener(loader);
+				}
+			});
+		}
+	};
+
+	// increase error timeout to 30 sec
+	yepnope.errorTimeout = 30000;
+
+}());
